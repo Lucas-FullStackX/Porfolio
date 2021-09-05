9edf0bb1a6c51393ffc3aafab91b61cf0b4ff74c
Lucas-FullStackX <lucasleguizamo21@gmail.com>
2021-09-05 10:58:40 -0500

    update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -37,7 +37,7 @@ const Footer = () => {
         <div
           onClick={linkendin}
           className={styles.linkedin}
-          data-aos="fade-left"
+          data-aos="fade-right"
         >
           <AiFillLinkedin />
           <h3>LinkedIn</h3>
